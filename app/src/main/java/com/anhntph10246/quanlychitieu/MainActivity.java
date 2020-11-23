e439a173ae55a3a2489ac17c179a3a0431eb7a16
somfan3 <somfan3@gmail.com>
2020-11-23 22:21:56 +0700

    comit

--- app/src/main/java/com/anhntph10246/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/anhntph10246/quanlychitieu/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //a
         setTitle("");
 
         nav = findViewById(R.id.bottom_naviagtionview);
